5cc837575129e04d75d73d241ac5cf1dee10bd0c
waterbear1996 <58069065+waterbear1996@users.noreply.github.com>
2020-12-23 23:38:33 +0800

    Add files via upload

---
+++ HW1: collect at least two customized rule-based patterns with indices/Main_r08723061_邱子軒.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+from random import sample 
+
+#from Process import Process
+#from Detect import Detect
+#from util.util_pattern import pattern
+
+pd.set_option('display.max_columns', 1000)
